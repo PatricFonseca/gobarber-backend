6e617d648845374b1d8c4aba6be8e92cb8ebdb13
PatricFonseca <patric.ccomp@gmail.com>
2019-07-16 21:40:13 -0300

    feature: Finish user register

---
+++ src/app/controllers/userController.js
@@ -0,0 +1,22 @@
+import User from '../models/user';
+
+class UserController {
+  async store(req, res) {
+    const userExits = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExits) {
+      return res.status(400).json({ error: 'User already exits' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,20 @@
 import { Router } from 'express';
+// import User from './app/models/user';
+import UserController from './app/controllers/userController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Funcionou' }));
+/*
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Patric de Paiva',
+    email: 'patric.ccomp@gmail.com',
+    password_hash: 2304924924,
+  });
 
+  return res.json(user);
+});
+*/
+
+routes.post('/users', UserController.store);
 export default routes;
