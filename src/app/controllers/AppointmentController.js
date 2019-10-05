9b2dc1431449473de4b6711822c703d4711a1a5a
patricfonseca <patric.ccomp@gmail.com>
2019-10-05 13:50:02 -0300

    start of available hours of provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,9 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -121,7 +124,20 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
     if (
       appointment.user_id !== req.userId &&
@@ -143,6 +159,10 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Queue.add(CancellationMail.key, {
+      appointment,
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '02a49ded567923',
+    pass: '591fe6d7eba67c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Outros serviços de e-mail
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * mandril(mailChimp)
+ * MailTrap -> Em uso(gratuito)
+ */
