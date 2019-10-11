e38f6b551cf984cf98a6390b9cae4edf7670587d
patricfonseca <patric.ccomp@gmail.com>
2019-10-10 22:05:12 -0300

    Finished available hours

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,4 +1,12 @@
-import { startOfDay, endOfDay } from 'date-fns';
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 
@@ -9,7 +17,11 @@ class AvailableController {
     if (!date) {
       return res.status(400).json({ error: 'Invalid date' });
     }
-
+    // 11/10/2019 07:00:02 --> 1570788002776
+    /**
+     * To get a date, go on chrome on console and
+     * code: new Date().getTime()
+     */
     const searchDate = Number(date);
 
     const appointments = await Appointment.findAll({
@@ -22,7 +34,38 @@ class AvailableController {
       },
     });
 
-    return res.json(appointments);
+    // TODO: Verificar configuração do usuário para carregar essa lista.
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const avaiable = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+      console.log(value);
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+    return res.json(avaiable);
   }
 }
 
